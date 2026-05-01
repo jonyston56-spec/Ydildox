96157f1e2d83cb4f5fb2676ad38b38fdb6580e52
jonyston56-spec <jonyston56@gmail.com>
2026-05-01 10:00:21 +0330

    index.js

---
+++ api/index.js
@@ -0,0 +1,18 @@
+export const config = { runtime: "edge" };
+
+const _0x5a1e = [
+  'TARGET_DOMAIN', 'replace', 'host', 'connection', 'keep-alive', 
+  'proxy-authenticate', 'proxy-authorization', 'te', 'trailer', 
+  'transfer-encoding', 'upgrade', 'forwarded', 'x-forwarded-host', 
+  'x-forwarded-proto', 'x-forwarded-port', 'x-vercel-', 'x-real-ip', 
+  'x-forwarded-for', 'GET', 'HEAD', 'half', 'manual', 'Bad Gateway: Tunnel Failed'
+];
+
+const _0x4f2c = (_0x3d1a) => _0x5a1e[_0x3d1a];
+
+const _0x2b8e1f = (process.env[_0x4f2c(0)] || "")[_0x4f2c(1)](/\/$/, "");
+
+const _0x1a9c3d = new Set([
+  _0x4f2c(2), _0x4f2c(3), _0x4f2c(4), _0x4f2c(5), _0x4f2c(6), 
+  _0x4f2c
+  
